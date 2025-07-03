1c95ebafc980aefb12c32585fbdc746be5b38f34
tanusree0 <tanusree6060@gmail.com>
2025-07-03 13:36:49 +0530

    ADD DYnamic port for deployment

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, () => {
-  console.log("Server running at http://localhost:8000");
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
